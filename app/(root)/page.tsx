4f6d582342465eb9a3cd7c2c94d6abc4176d1cbf
Nitish Kafle <kaflenit@gmail.com>
2023-12-17 12:58:33 -0600

    debounce for dynamic query for search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
@@ -45,6 +46,7 @@ export default async function Home() {
           Trusted by <br /> Thousands of Events
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search placeholder="Search events..." />
           Search CategoryFilter
         </div>
 
