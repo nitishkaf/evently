fcbdd7b80860cd4a8a86dadabba0fd716b97df33
Nitish Kafle <kaflenit@gmail.com>
2023-12-17 13:01:55 -0600

    fix get all event server actions

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -14,6 +14,10 @@ import Event from "../database/models/event.model";
 import Category from "../database/models/category.model";
 import { revalidatePath } from "next/cache";
 
+const getCategoryByName = async (name: string) => {
+  return Category.findOne({ name: { $regex: name, $options: "i" } });
+};
+
 const populateEvent = async (query: any) => {
   return query
     .populate({
@@ -69,22 +73,35 @@ export const getEventsById = async (eventId: string) => {
   }
 };
 
-export const getAllEvents = async ({
+export async function getAllEvents({
   query,
   limit = 6,
   page,
   category,
-}: GetAllEventsParams) => {
+}: GetAllEventsParams) {
   try {
     await connectToDatabase();
-    const conditions = {};
+
+    const titleCondition = query
+      ? { title: { $regex: query, $options: "i" } }
+      : {};
+    const categoryCondition = category
+      ? await getCategoryByName(category)
+      : null;
+    const conditions = {
+      $and: [
+        titleCondition,
+        categoryCondition ? { category: categoryCondition._id } : {},
+      ],
+    };
+
+    const skipAmount = (Number(page) - 1) * limit;
     const eventsQuery = Event.find(conditions)
       .sort({ createdAt: "desc" })
-      .skip(0)
+      .skip(skipAmount)
       .limit(limit);
 
     const events = await populateEvent(eventsQuery);
-
     const eventsCount = await Event.countDocuments(conditions);
 
     return {
@@ -94,7 +111,7 @@ export const getAllEvents = async ({
   } catch (error) {
     handleError(error);
   }
-};
+}
 
 export async function updateEvent({ userId, event, path }: UpdateEventParams) {
   try {
